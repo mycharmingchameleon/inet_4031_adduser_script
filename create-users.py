db8044102ce1e2b4b9b429cab45f395b8d82cc20
Mridul Shrestha <mridul.stha@gmail.com>
2025-10-30 20:30:42 +0000

    Add create-users.py

--- create-users.py
+++ create-users.py
@@ -21,11 +21,10 @@ def main():
         #re is checking if the line starts wiht '#'
         #lines starting with '#' should be ignored becuase it is a comment
         match = re.match("^#",line)
-
+        print(match)
         #stripes() removes whitelines, split(':') divides each lines into feilds
         fields = line.strip().split(':')
-
-       
+        print("Field length: ", len(fields))
         #what would an appropriate comment be for describing what this IF statement is checking for? - if the username matches
         #what happens if the IF statement evaluates to true? - it continues and moves onto the next line the loop that is.
         #how does this IF statement rely on what happened in the prior two lines of code? The match and fields lines. - it checks if its a
@@ -39,7 +38,7 @@ def main():
 	#username = fields[0] - login name
 	#password = fields[1] - password
         #fields[2] and fields[3] contain first and last name info for GECOS
-	username = fields[0]
+        username = fields[0]
         password = fields[1]
         gecos = "%s %s,,," % (fields[3],fields[2])
 
@@ -52,8 +51,8 @@ def main():
         cmd = "/usr/sbin/adduser --disabled-password --gecos '%s' %s" % (gecos,username)
 
         #First time it displays command for verification then the os.system(cmd) is uncommented and executued once verified.
-        #print cmd
-        #os.system(cmd)
+        print (cmd)
+        os.system(cmd)
 
         #status message about setting the user's password.
         print("==> Setting the password for %s..." % (username))
@@ -62,16 +61,16 @@ def main():
         cmd = "/bin/echo -ne '%s\n%s' | /usr/bin/sudo /usr/bin/passwd %s" % (password,password,username)
 
         #test by printing the command before enabling actual execution.
-        #print cmd
-        #os.system(cmd)
+        print (cmd)
+        os.system(cmd)
 
         for group in groups:
             #Skip adding the user to a group if the group name is '-'
             if group != '-':
                 print("==> Assigning %s to the %s group..." % (username,group))
                 cmd = "/usr/sbin/adduser %s %s" % (username,group)
-                #print cmd
-                #os.system(cmd)
+                print (cmd)
+                os.system(cmd)
 
 if __name__ == '__main__':
     main()
